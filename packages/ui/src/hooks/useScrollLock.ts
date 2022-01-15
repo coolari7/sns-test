914c117e28df4afc59d9f343412826cc40c51422
Arijit Chowdhury <coolari7@gmail.com>
2022-01-15 21:26:44 +0530

    Add /hooks as a dependency for ui

--- packages/ui/src/hooks/useScrollLock.ts
+++ packages/ui/src/hooks/useScrollLock.ts
@@ -1,10 +1,1 @@
-export function useScrollLock() {
-  return {
-    lock(elem: HTMLElement = document.body) {
-      elem.style.overflow = "hidden";
-    },
-    unlock(elem: HTMLElement = document.body) {
-      elem.style.removeProperty("overflow");
-    },
-  };
-}
+export { useScrollLock } from "@thenamesarijit/hooks";
